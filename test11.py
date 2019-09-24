a20847fd49d58c0c256fcd4f170d0a7ee49b43ba
AlhasanAlkhatib <37053503+AlhasanAlkhatib@users.noreply.github.com>
2019-09-24 16:52:28 +0300

    Create test11.py
    using converting command

---
+++ test11.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(70, 80, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.toolButton = QtWidgets.QToolButton(Form)
+        self.toolButton.setGeometry(QtCore.QRect(150, 160, 25, 19))
+        self.toolButton.setObjectName("toolButton")
+        self.verticalSlider = QtWidgets.QSlider(Form)
+        self.verticalSlider.setGeometry(QtCore.QRect(230, 70, 22, 160))
+        self.verticalSlider.setOrientation(QtCore.Qt.Vertical)
+        self.verticalSlider.setObjectName("verticalSlider")
+        self.horizontalSlider = QtWidgets.QSlider(Form)
+        self.horizontalSlider.setGeometry(QtCore.QRect(220, 40, 160, 22))
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "PushButton"))
+        self.toolButton.setText(_translate("Form", "..."))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
